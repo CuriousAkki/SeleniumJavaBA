c54a70cdd308b5b2e55e7a3a19138be2285c31a8
CuriousAkki <86379774+CuriousAkki@users.noreply.github.com>
2021-06-25 10:57:45 +0530

    Add files via upload

---
+++ Day2/JavaBrushup/src/Base.java
@@ -0,0 +1,20 @@
+
+public class Base {
+	
+	
+	public void validateHeader() {
+		
+		System.out.println("validated header method");
+	}
+	
+ public int intejer() {
+		
+		return 3;
+	}
+ 
+ public String Stringrtuen() {
+		
+		return "Ishtring";
+	}
+
+}
---
+++ Day2/JavaBrushup/src/Basics.java
@@ -0,0 +1,28 @@
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Selenium Programmer");
+		
+		int a=20,b=30;
+		
+		int c = a + b;
+		
+		System.out.println("sum is :"+" "+c);
+		
+		Base d = new Base();
+		
+		d.validateHeader();
+		
+		int e  = d.intejer();
+		System.out.println(e);
+		
+		String t = d.Stringrtuen();
+		System.out.println(t);
+		
+
+	}
+
+}
